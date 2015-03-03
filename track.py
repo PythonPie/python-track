60308dccfd59d05412dbb34d6e78e0fa23b92e9b
PythonPie <vijayshekar.hs@gmail.com>
2015-03-04 00:24:59 +0530

    introduction to output excel sheet creation

--- track.py
+++ track.py
@@ -3,6 +3,15 @@ args = {}
 
 try:
 	args = parse.parse_args()
+	if args['sheet'][-4:] == '.xls' :
+		QuerySheet = open(args['sheet'],'w')
+		myString = "I have passed valid excel sheet as argument"
+		QuerySheet.write(myString)	
+	else:
+		print("passed excel format is not correct, creating default sheet")
+		QuerySheet = open(args['user']+'.xls','w')
+		myString = "I am learning python \n "
+		QuerySheet.write(myString)			
 except BaseException as e:
 	pass
 	# print(e)
